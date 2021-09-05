3db49458361a04e461093b01af5dc1fd60b2b810
JimenaKatuMartinez <93katu@gmail.com>
2021-09-05 20:15:54 -0300

    Listo funcion agregar producto a lista izquierda

--- estilos/index.js
+++ estilos/index.js
@@ -1,33 +1,53 @@
-let producto = document.getElementsByClassName("producto");
+let descripcion = document.getElementsByClassName("descripcion");
+let productos = document.getElementsByClassName("producto");
+let precioProducto = document.getElementsByClassName("producto-precio");
 let table = document.querySelector("table");
 let tbody = document.querySelector("tbody");
-let listaPedidos = [];
+let listaPedidos = JSON.parse(localStorage.getItem("productos")) || [];
 
-for (let i = 0; i < producto.length; i++) {
-  producto[i].addEventListener("click", createItem);
+//se supone que es para crear un arreglo con el precio y producto
+const crearItem = (producto, precio) => {
+  let item = {
+    producto: producto,
+    precio: precio,
+  };
+  listaPedidos.push(item);
+  console.log(listaPedidos);
+  return item;
+};
 
-  function createItem(event) {
-    listaPedidos.push(event.target.innerHTML);
+//Insertar nombre del producto al array "listaPedidos" al hacer click spbre el
+for (let i = 0; i < descripcion.length; i++) {
+  descripcion[i].addEventListener("click", insertarItem);
+  let producto = productos[i].innerHTML;
+  let precio = precioProducto[i].innerHTML;
+  console.log(producto);
 
-    guardarLS(listaPedidos);
-    leerLS()
+  function insertarItem(event) {
+    console.log(listaPedidos);
+    crearItem(producto, precio);
+    guardarLS();
   }
 }
 
-const guardarLS = ()=>{
- localStorage.setItem('producto', JSON.stringify(listaPedidos))
-}
+//Guardar producto de "listaPedidos" en LocalStorage
+const guardarLS = () => {
+  localStorage.setItem("productos", JSON.stringify(listaPedidos));
+  leerLS();
+};
 
-//document.addEventListener('DOMContentLoaded', leerLS)
-
-const leerLS = () =>{
-    listaPedidos = JSON.parse(localStorage.getItem('producto'))
-    if(listaPedidos === null){
-        listaPedidos = []
-    }else{
-        listaPedidos.forEach(element => {
-            console.log(element)
-            tbody.innerHTML += `<td>${listaPedidos}</td>`
-        });
-    }
-    }
+//Insertar en HTML el item al hacer click spbre el [ARREGLAR]
+const leerLS = () => {
+  listaPedidos = JSON.parse(localStorage.getItem("productos"));
+  console.log(listaPedidos);
+  if (listaPedidos === null) {
+    listaPedidos = [];
+  } else {
+    tbody.innerHTML = "";
+    listaPedidos.forEach((element) => {
+      tbody.innerHTML += `<td>${element.producto}</td>
+      <td>${element.precio}</td>`;
+    });
+  }
+};
+leerLS();
